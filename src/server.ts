15014e112e7b4a88f79402e441e8b3b41d090de1
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-07-10 00:44:55 -0400

    feat: Delete image after response is sent

--- src/server.ts
+++ src/server.ts
@@ -14,20 +14,7 @@ import validUrl from 'valid-url';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
+  // endpoint to get the image, process it and return the filtered image
   app.get("/filteredimage/", async (req, res) => {
     try {
       let { image_url } = req.query;
@@ -48,21 +35,20 @@ import validUrl from 'valid-url';
       }
 
       // Process the image
-      const imgPath = await filterImageFromURL(image_url);
+      let imgPath = await filterImageFromURL(image_url);
       if(imgPath) {
-        await res.status(200).sendFile(imgPath);
+        res.on('finish', () => deleteLocalFiles([imgPath]));
+        return res.status(200).sendFile(imgPath);
       } else {
-        res.status(500).send({error: 'Unable to process your request'});
+        return res.status(500).send({error: 'Unable to process your request'});
       }
 
     } catch (e) {
       console.log(e)
-      res.status(500).send({error: 'Unable to process your request'})
+      return res.status(500).send({error: 'Unable to process your request'})
     }
   });
-  /**************************************************************************** */
 
-  //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user

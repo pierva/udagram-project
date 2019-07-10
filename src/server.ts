ffe85e11538a9328a63038dbe5ff347f7f153eb5
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-07-10 00:04:26 -0400

    feat: Receive and process the image

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFiles, validateExtension} from './util/util';
+import validUrl from 'valid-url';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -27,16 +28,48 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  app.get("/filteredimage/", async (req, res) => {
+    try {
+      let { image_url } = req.query;
+      // Check if the user has entered any query parameter
+      if (!image_url) {
+        return res.status(400)
+          .send({error: 'Please specify image_url as query parameter'});
+      }
+
+      // If we have a query parameter, check if it is a valid url
+      if(!validUrl.isUri(image_url)){
+        return res.status(400).send({error: 'Please enter a valid url'});
+      }
+
+      // Check if the file is an image
+      if (!validateExtension(image_url)) {
+        return res.status(415).send({error: 'Invalid media type'});
+      }
+
+      // Process the image
+      const imgPath = await filterImageFromURL(image_url);
+      if(imgPath) {
+        await res.status(200).sendFile(imgPath);
+      } else {
+        res.status(500).send({error: 'Unable to process your request'});
+      }
+
+    } catch (e) {
+      console.log(e)
+      res.status(500).send({error: 'Unable to process your request'})
+    }
+  });
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {

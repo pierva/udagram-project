d85dcb039837961fe44e6732d46112e619f9b3b2
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-07-11 06:32:00 -0400

    fix: Python subprocess call

--- src/util/child-process.ts
+++ src/util/child-process.ts
@@ -1,16 +1,23 @@
-import { spawn } from 'child_process';
+import {spawn} from "child_process";
+const path = require('path');
 
 
 export async function findEdges(imgPath: string): Promise<string> {
   return new Promise(async resolve => {
-    const pythonProcess = spawn('python3',
-    ["./opencvfilter.py"]);
-
-      if (pythonProcess !== undefined) {
-
-          pythonProcess.stdout.on('data', (data) => {
-            console.log(data.toString());
-          });
-      }
+    const pythonProcess = spawn('python3',[
+        path.join(__dirname, "opencvfilter.py"),
+        '-i', imgPath
+      ]);
+    if (pythonProcess !== undefined) {
+        pythonProcess.stdout.on('data', (data) => {
+          resolve(data.toString());
+        });
+        process.stderr.on('data', function (data){
+          console.log(data.toString());
+          resolve(data);
+        });
+    } else {
+      reject('Unable to process the image in python');
+    }
   });
 }

cc4c442f2bc8914213425337ace13fe26070e4b4
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-07-12 05:54:06 -0400

    feat: Add login route

--- src/controllers/users/routes/auth.router.ts
+++ src/controllers/users/routes/auth.router.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/User';
 
 import * as bcrypt from 'bcrypt';
-
+import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
@@ -21,12 +21,47 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
     return await bcrypt.compare(plainTextPassword, hash);
 }
 
+function generateJWT(user: User): string {
+    return jwt.sign(user.toJSON(), config.jwt.secret);
+}
+
 router.get('/verification',
     requireAuth,
     async (req: Request, res: Response) => {
         return res.status(200).send({ auth: true, message: 'Authenticated.' });
 });
 
+router.post('/login', async (req: Request, res: Response) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // check email is valid
+    if (!email || !EmailValidator.validate(email)) {
+        return res.status(400).send({ auth: false, message: 'Email is required or malformed' });
+    }
+
+    // check email password valid
+    if (!password) {
+        return res.status(400).send({ auth: false, message: 'Password is required' });
+    }
+
+    const user = await User.findByPk(email);
+    // check that user exists
+    if(!user) {
+        return res.status(401).send({ auth: false, message: 'Unauthorized' });
+    }
+
+    // check that the password matches
+    const authValid = await comparePasswords(password, user.password_hash)
+
+    if(!authValid) {
+        return res.status(401).send({ auth: false, message: 'Unauthorized' });
+    }
+
+    // Generate JWT
+    const jwt = generateJWT(user);
+
+    res.status(200).send({ auth: true, token: jwt, user: user.short()});
+});
 
 router.get('/', async (req: Request, res: Response) => {
     res.send('auth')

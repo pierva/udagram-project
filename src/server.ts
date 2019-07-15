b52664968f6493adaf0a66e7ef754e95ea7bd82b
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-07-15 10:45:44 -0400

    refactor: Update status codes

--- src/server.ts
+++ src/server.ts
@@ -21,15 +21,16 @@ import { findEdges } from './util/child-process';
       let { image_url } = req.query;
       let { lower } = req.query;
       let { upper } = req.query;
+
       // Check if the user has entered any query parameter
       if (!image_url) {
-        return res.status(400)
+        return res.status(422)
           .send({error: 'Please specify image_url as query parameter'});
       }
 
       // If we have a query parameter, check if it is a valid url
       if(!validUrl.isUri(image_url)){
-        return res.status(400).send({error: 'Please enter a valid url'});
+        return res.status(415).send({error: 'Please enter a valid url'});
       }
 
       // Process the image
